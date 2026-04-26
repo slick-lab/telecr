eaa73944e355e891098543fb95a76fce22bb0493
ZenDrx <ynwghosted@icloud.com>
2026-04-26 07:13:23 +0100

    Change bot token in recieves_command.cr
    Updated bot token with a revoked token for testing purposes.

--- test/recieves_command.cr
+++ test/recieves_command.cr
@@ -1,6 +1,6 @@
 require "../src/telecr"
 
-bot = Telecr.new("8759339325:AAGSCZViV9QlBqWfCYDXguPa477a4WFKaXA")
+bot = Telecr.new("8759339325:AAGSCZhgViV9QlBqWfCYDXguPa4f77a4WFKaXA") //fake token btw have revoked it 😂 try your luck 🤧
 
 bot.command("start") do |ctx|
     ctx.reply("Hello! I am your friendly neighborhood bot. How can I assist you today?")
